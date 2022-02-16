5587b56fa3a1651b388b337970cac29305bf7308
Szalai Alexandra <szandiszalai09@gmail.com>
2022-02-16 09:58:43 +0100

    Kezdes

---
+++ App.java
@@ -0,0 +1,34 @@
+/*
+* File: App.java
+* Author: Szalai Alexandra
+* Copyright: 2022, Szalai Alexandra
+* Group: Szoft I N
+* Date: 2022-02-16
+* Github: https://github.com/szalaialexandra/
+* Licenc: GNU GPL
+*/
+
+import java.util.Scanner;
+
+class App {
+	public static void main (String [] args){
+		System.out.println("Szalai Alexandra, 2022_02_16");
+		System.out.println("Feladat 0306");
+		System.out.println("Háromszog terulete");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Oldal: ");
+		int oldal = sc.nextInt();
+		
+		System.out.println("Magassag: ");
+		double magassag = sc.nextDouble();
+		
+		double terulet =
+		
+		
+		
+		
+		
+	}
+}
+
